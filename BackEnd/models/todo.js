37af07c1a11f6a3ad9d5b8f7be93cfd004e8a5e4
Gabe Denton <mason.g.denton@vanderbilt.edu>
2023-04-04 14:28:47 -0500

    template

--- BackEnd/models/todo.js
+++ BackEnd/models/todo.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
 const todoSchema = mongoose.Schema({
-    title: { type: String, require: true},
-    content: { type: String, require: true},
-    date: { type: String, require: true},
+
+    title: { type: String, required: true},
+    content: { type: String, required: true},
+    date: { type: String, required: true},
+    
 });
 
 module.exports = mongoose.model("Todo", todoSchema);
