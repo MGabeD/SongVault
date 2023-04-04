e5819b9f19b366981f81b73d13fd17d364d9e5aa
Gabe Denton <mason.g.denton@vanderbilt.edu>
2023-04-04 18:40:40 -0500

    a

--- BackEnd/models/conversation.js
+++ BackEnd/models/conversation.js
@@ -1,20 +1,22 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const conversationSchema = mongoose.Schema({
+const conversationSchema = mongoose.Schema({
     
-//     users : { type: [String], default: [], required: true },
-//     messages : { type: [{ sender: { 
-//         type: String, required: true, 
-//         body: { type: String, required: true,
-//          } } }], default: [], required: true}
-//     // firstName : { type: String, required: true },
-//     // lastName : { type: String, required: true },
-//     // email : { type: String, required: true },
-//     // stageName : { type: String, required: true },
-//     // birthday : {type: String, required: true },
-//     // bio : {type: String, required: true },
-//     // hyperLinks: { type: [String], default: [], required: true },
+    users : { type: [String], default: [], required: true },
+    title : { type: String, default: "New Group Chat", required: true },
+    // image : { id: String },
+    // messages : { type: [{ sender: { 
+    //     type: String, required: true, 
+    //     body: { type: String, required: true,
+    //      } } }], default: [], required: true}
+    // firstName : { type: String, required: true },
+    // lastName : { type: String, required: true },
+    // email : { type: String, required: true },
+    // stageName : { type: String, required: true },
+    // birthday : {type: String, required: true },
+    // bio : {type: String, required: true },
+    // hyperLinks: { type: [String], default: [], required: true },
 
-// });
+});
 
-// module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("Conversation", conversationSchema);
