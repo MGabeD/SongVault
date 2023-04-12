bdc2142f5c1a4cecfd29d1320f6179213dff71c0
Gabe Denton <mason.g.denton@vanderbilt.edu>
2023-04-12 00:22:51 -0500

    model-song

--- BackEnd/models/song.js
+++ BackEnd/models/song.js
@@ -5,7 +5,6 @@ const songSchema = mongoose.Schema({
     title: { type: String, required: true },
     artist: { type: [String], required: true },
     
-    
 });
 
 module.exports = mongoose.model("Request", requestSchema);
