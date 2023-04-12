57bd552e5f36354ae8c105db254052404af5e33d
Wesley Minton <wesleyminton10@gmail.com>
2023-04-11 23:35:35 -0500

    fixing index.js bug

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -188,10 +188,7 @@ app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
-    // res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    // next();
+    next();
 });
 
 // app.use(bodyParser.urlencoded( { extended: false } ));
