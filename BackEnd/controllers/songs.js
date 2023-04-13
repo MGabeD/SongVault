1bf5badfd30cdf871139cba5e73fd8e550f2b9f0
Gabe Denton <mason.g.denton@vanderbilt.edu>
2023-04-13 00:18:30 -0500

    fixed urls?

--- BackEnd/controllers/songs.js
+++ BackEnd/controllers/songs.js
@@ -17,113 +17,210 @@ admin.initializeApp({
   const storage = multer.memoryStorage();
   const upload = multer({ storage: storage });
 
-
-exports.createSong = async (req, res, next) => {
-  try {
-    upload.fields([
-      { name: "Audio", maxCount: 1 },
-      { name: "Image", maxCount: 1 },
-    ])(req, res, async (err) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send("Failed to upload files");
-      }
-
-      if (!req.files || !req.files["Audio"] || !req.files["Image"]) {
-        return res.status(400).json({ message: "Missing files" });
-      }
-
-      const audioFile = req.files["Audio"][0];
-      const imageFile = req.files["Image"][0];
-
-      const song = new Song({
-        name: req.body.songName,
-        artist: req.body.user,
-        likes: req.body.likes,
-        plays: req.body.plays,
-      });
-
-      const savedSong = await song.save();
-      console.log(savedSong._id);
-
-      const audioFileObject = bucket.file(
-        `${savedSong._id}/${audioFile.originalname}`
-      );
-      const audioStream = audioFileObject.createWriteStream({
-        metadata: {
-          contentType: audioFile.mimetype,
-        },
-      });
-
-      audioStream.on("error", (err) => {
-        console.error(err);
-        res.status(500).send("Failed to upload audio file");
-      });
-
-      audioStream.on("finish", async () => {
-        const imageFileObject = bucket.file(
-          `${savedSong._id}/${imageFile.originalname}`
+  exports.createSong = async (req, res, next) => {
+    try {
+      upload.fields([
+        { name: "Audio", maxCount: 1 },
+        { name: "Image", maxCount: 1 },
+      ])(req, res, async (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send("Failed to upload files");
+        }
+  
+        if (!req.files || !req.files["Audio"] || !req.files["Image"]) {
+          return res.status(400).json({ message: "Missing files" });
+        }
+  
+        const audioFile = req.files["Audio"][0];
+        const imageFile = req.files["Image"][0];
+  
+        const song = new Song({
+          name: req.body.songName,
+          artist: req.body.user,
+          likes: req.body.likes,
+          plays: req.body.plays,
+        });
+  
+        const savedSong = await song.save();
+        console.log(savedSong._id);
+  
+        const audioFileObject = bucket.file(
+          `${savedSong._id}/${audioFile.originalname}`
         );
-        const imageStream = imageFileObject.createWriteStream({
+        const audioStream = audioFileObject.createWriteStream({
           metadata: {
-            contentType: imageFile.mimetype,
+            contentType: audioFile.mimetype,
           },
         });
-
-        imageStream.on("error", (err) => {
+  
+        audioStream.on("error", (err) => {
           console.error(err);
-          res.status(500).send("Failed to upload image file");
+          res.status(500).send("Failed to upload audio file");
         });
-
-        imageStream.on("finish", async () => {
-          try {
-            savedSong.mp3Link = `gs://${audioFileObject.bucket.name}/${audioFileObject.name}`;
-            savedSong.imageLink = `gs://${imageFileObject.bucket.name}/${imageFileObject.name}`;
-            console.log(savedSong.imageFile)
-            // const updatedSong = await savedSong.save();
-            const upSong = {
-                // name: savedSong.name,
-                // artist: savedSong.artist,
-                // likes: savedSong.likes,
-                // plays: savedSong.plays,
+  
+        audioStream.on("finish", async () => {
+          const imageFileObject = bucket.file(
+            `${savedSong._id}/${imageFile.originalname}`
+          );
+          const imageStream = imageFileObject.createWriteStream({
+            metadata: {
+              contentType: imageFile.mimetype,
+            },
+          });
+  
+          imageStream.on("error", (err) => {
+            console.error(err);
+            res.status(500).send("Failed to upload image file");
+          });
+  
+          imageStream.on("finish", async () => {
+            try {
+              await Promise.all([
+                audioFileObject.makePublic(),
+                imageFileObject.makePublic(),
+              ]);
+  
+              savedSong.mp3Link = `https://storage.googleapis.com/${audioFileObject.bucket.name}/${audioFileObject.name}`;
+              savedSong.imageLink = `https://storage.googleapis.com/${imageFileObject.bucket.name}/${imageFileObject.name}`;
+  
+              const upSong = {
                 imageLink: savedSong.imageLink,
-                mp3Link: savedSong.mp3Link
+                mp3Link: savedSong.mp3Link,
+              };
+              await Song.updateOne({ _id: savedSong._id }, { $set: upSong });
+  
+              res.status(201).json({
+                message: "Song created successfully",
+                song: upSong,
+              });
+            } catch (error) {
+              console.error(error);
+              res.status(500).json({
+                message: "Failed to save song with URLs",
+              });
             }
-            console.log(upSong);
-            Song.updateOne({ _id: savedSong._id }, { $set: upSong })
-                .then((result) => {
-                    // res.status(200).json({ message: "Update is successful!" });
-                })
-                .catch((error) => {
-                    res.status(500).json({
-                        message: "Couldn't update Song!",
-                    });
-                    console.log(error);
-                });
-            res.status(201).json({
-              message: "Song created successfully",
-              song: upSong,
-            });
-          } catch (error) {
-            console.error(error);
-            res.status(500).json({
-              message: "Failed to save song with URLs",
-            });
-          }
+          });
+  
+          imageStream.end(imageFile.buffer);
         });
-
-        imageStream.end(imageFile.buffer);
+  
+        audioStream.end(audioFile.buffer);
       });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: "An error occurred while creating the song",
+      });
+    }
+  };
+  
+// exports.createSong = async (req, res, next) => {
+//   try {
+//     upload.fields([
+//       { name: "Audio", maxCount: 1 },
+//       { name: "Image", maxCount: 1 },
+//     ])(req, res, async (err) => {
+//       if (err) {
+//         console.error(err);
+//         return res.status(500).send("Failed to upload files");
+//       }
 
-      audioStream.end(audioFile.buffer);
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "An error occurred while creating the song",
-    });
-  }
-};
+//       if (!req.files || !req.files["Audio"] || !req.files["Image"]) {
+//         return res.status(400).json({ message: "Missing files" });
+//       }
+
+//       const audioFile = req.files["Audio"][0];
+//       const imageFile = req.files["Image"][0];
+
+//       const song = new Song({
+//         name: req.body.songName,
+//         artist: req.body.user,
+//         likes: req.body.likes,
+//         plays: req.body.plays,
+//       });
+
+//       const savedSong = await song.save();
+//       console.log(savedSong._id);
+
+//       const audioFileObject = bucket.file(
+//         `${savedSong._id}/${audioFile.originalname}`
+//       );
+//       const audioStream = audioFileObject.createWriteStream({
+//         metadata: {
+//           contentType: audioFile.mimetype,
+//         },
+//       });
+
+//       audioStream.on("error", (err) => {
+//         console.error(err);
+//         res.status(500).send("Failed to upload audio file");
+//       });
+
+//       audioStream.on("finish", async () => {
+//         const imageFileObject = bucket.file(
+//           `${savedSong._id}/${imageFile.originalname}`
+//         );
+//         const imageStream = imageFileObject.createWriteStream({
+//           metadata: {
+//             contentType: imageFile.mimetype,
+//           },
+//         });
+
+//         imageStream.on("error", (err) => {
+//           console.error(err);
+//           res.status(500).send("Failed to upload image file");
+//         });
+
+//         imageStream.on("finish", async () => {
+//           try {
+//             savedSong.mp3Link = `gs://${audioFileObject.bucket.name}/${audioFileObject.name}`;
+//             savedSong.imageLink = `gs://${imageFileObject.bucket.name}/${imageFileObject.name}`;
+//             console.log(savedSong.imageFile)
+//             // const updatedSong = await savedSong.save();
+//             const upSong = {
+//                 // name: savedSong.name,
+//                 // artist: savedSong.artist,
+//                 // likes: savedSong.likes,
+//                 // plays: savedSong.plays,
+//                 imageLink: savedSong.imageLink,
+//                 mp3Link: savedSong.mp3Link
+//             }
+//             console.log(upSong);
+//             Song.updateOne({ _id: savedSong._id }, { $set: upSong })
+//                 .then((result) => {
+//                     // res.status(200).json({ message: "Update is successful!" });
+//                 })
+//                 .catch((error) => {
+//                     res.status(500).json({
+//                         message: "Couldn't update Song!",
+//                     });
+//                     console.log(error);
+//                 });
+//             res.status(201).json({
+//               message: "Song created successfully",
+//               song: upSong,
+//             });
+//           } catch (error) {
+//             console.error(error);
+//             res.status(500).json({
+//               message: "Failed to save song with URLs",
+//             });
+//           }
+//         });
+
+//         imageStream.end(imageFile.buffer);
+//       });
+
+//       audioStream.end(audioFile.buffer);
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({
+//       message: "An error occurred while creating the song",
+//     });
+//   }
+// };
 
 exports.updatedSong = (req, res, next) => {
     const updatedSong = {
