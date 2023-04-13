597e8b0205167537482b688613496c9645ac1567
Gabe Denton <mason.g.denton@vanderbilt.edu>
2023-04-12 23:50:47 -0500

    new model

--- BackEnd/models/song.js
+++ BackEnd/models/song.js
@@ -5,7 +5,9 @@ const songSchema = mongoose.Schema({
     name: { type: String, required: true },
     artist: { type: [String], required: true },
     likes: { type: Number, default: 0, required: true },
-    plays: { type: Number, default: 0, required: true }
+    plays: { type: Number, default: 0, required: true },
+    imageLink: { type: String, required: false },
+    mp3Link: { type: String, required: false }
     
 });
 
